79be0472061dbb8657d19e09eb12620241f5d1cd
Nano <nanopp@gmail.com>
2017-05-30 11:31:31 -0600

    recipe list initiation

--- ClientApp/app/components/recipes/recipe-list/recipe-list.component.ts
+++ ClientApp/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 ﻿
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
+    recipes: Recipe[] = [
+        new Recipe('Cookie', 'This is simply a test', 'http://static.wixstatic.com/media/95a490_57eb937130ac42a780f50927cb34ec0e~mv2_d_1280_1280_s_2.jpg_256'),
+        new Recipe('Brownie', 'This is test 2', 'https://static.wixstatic.com/media/87f810_d83a1a2a408f4d2d8c12e082dda020f0~mv2.jpg_256')
+    ];
+
+    constructor() {
+        
+    }
+
+    ngOnInit() {
+        
+    }
 }
