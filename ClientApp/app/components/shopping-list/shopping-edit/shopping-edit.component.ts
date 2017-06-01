7d2590a3570485c2e6d5518d85c90fc4ff3f72d4
Nano <nanopp@gmail.com>
2017-06-01 15:39:22 -0600

    Add items

--- ClientApp/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ClientApp/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,21 @@
 ﻿
-import { Component } from '@angular/core';
+import { Component, Output, ElementRef, ViewChild, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model'
 
 @Component({
     selector: 'app-shopping-edit',
     templateUrl: './shopping-edit.component.html'
 })
 export class ShoppingEditComponent {
+    @ViewChild('nameInput') nameInputRef: ElementRef;
+    @ViewChild('amountInput') amountInputRef: ElementRef;
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+    onAddItem() {
+        const ingName = this.nameInputRef.nativeElement.value;
+        const ingAmount = this.amountInputRef.nativeElement.value;
+        const newIngredient = new Ingredient(ingName, ingAmount);
+
+        this.ingredientAdded.emit(newIngredient);
+    }
 }
