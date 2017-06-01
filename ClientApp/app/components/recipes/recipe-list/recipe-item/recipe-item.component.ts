bf9f241e509e34ca290b462650a6cbf508779e27
Nano <nanopp@gmail.com>
2017-06-01 14:44:27 -0600

    recipe item now functional with input parameter

--- ClientApp/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ClientApp/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,11 @@
 ﻿
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../../shared/recipe.model'
 
 @Component({
     selector: 'app-recipe-item',
     templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent {
-
+    @Input() recipe: Recipe;
 }
--- ClientApp/app/components/recipes/recipe-list/recipe-list.component.ts
+++ ClientApp/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 ﻿
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../../shared/recipe.model'
 
 @Component({
     selector: 'app-recipe-list',
